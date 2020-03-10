84eef25c0e53ab3ebc8abaef9152383327d1fd3d
briannalturner <blturner3527@gmail.com>
2020-03-10 15:15:36 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.getElementById('create-task-form')
+  form.addEventListener("submit", formHandler)
 });
+
+function formHandler(event) {
+  event.preventDefault();
+  console.log("default prevented")
+  let list = document.getElementById("tasks")
+  let newTask = document.createElement('li')
+  let input = document.getElementById('new-task-description').value
+  console.log(input)
+  newTask.innerText = `${input}`
+  list.appendChild(newTask)
+}
